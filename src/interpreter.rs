f89238243aa8624f22f3722f5dd33e71a18b6d55
Zhennan Tu <zhennan.tu@gmail.com>
2020-03-31 01:09:19 +0800

    unify error handling

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -7,76 +7,114 @@ pub struct Interpreter {
 }
 
 impl Interpreter {
-    fn lookup_name(&self, node: &ast::AstNode) -> ntable::Variable {
-        return self.name_table.get(&node.token.literal).unwrap();
+    fn lookup_variable(&self, node: &ast::AstNode) -> Result<i64, String> {
+        return match self.name_table.get(&node.token.literal) {
+            Ok(variable) => Ok(variable.value),
+            Err(msg) => Err(msg),
+        };
     }
 
-    fn exec_pow_expression(&self, node: &ast::AstNode) -> i64 {
-        let left = self.exec_expression(&node.childs[0]);
-        let right = self.exec_expression(&node.childs[1]);
+    fn exec_pow_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
+        let right = match self.exec_expression(&node.childs[1]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
         match node.token.token_type {
             tokenizer::TokenType::Pow => {
                 let mut result: i64 = 1;
                 for _ in 0..right {
                     result *= left;
                 }
-                return result;
+                return Ok(result);
             },
             _ => panic!("logic error"),
         }
     }
 
-    fn exec_mul_expression(&self, node: &ast::AstNode) -> i64 {
-        let left = self.exec_expression(&node.childs[0]);
-        let right = self.exec_expression(&node.childs[1]);
+    fn exec_mul_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
+        let right = match self.exec_expression(&node.childs[1]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
         match node.token.token_type {
-            tokenizer::TokenType::Mul => return left * right,
-            tokenizer::TokenType::Div => return left / right,
+            tokenizer::TokenType::Mul => return Ok(left * right),
+            tokenizer::TokenType::Div => return Ok(left / right),
             _ => panic!("logic error"),
         }
     }
 
-    fn exec_add_expression(&self, node: &ast::AstNode) -> i64 {
-        let left = self.exec_expression(&node.childs[0]);
-        let right = self.exec_expression(&node.childs[1]);
+    fn exec_add_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
+        let right = match self.exec_expression(&node.childs[1]) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
         match node.token.token_type {
-            tokenizer::TokenType::Add => return left + right,
-            tokenizer::TokenType::Sub => return left - right,
-            tokenizer::TokenType::Mod => return left % right,
+            tokenizer::TokenType::Add => return Ok(left + right),
+            tokenizer::TokenType::Sub => return Ok(left - right),
+            tokenizer::TokenType::Mod => return Ok(left % right),
             _ => panic!("logic error"),
         }
     }
 
-    fn exec_expression(&self, node: &ast::AstNode) -> i64 {
+    fn exec_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
         match node.node_type {
             ast::NodeType::Add => return self.exec_add_expression(node),
             ast::NodeType::Mul => return self.exec_mul_expression(node),
             ast::NodeType::Pow => return self.exec_pow_expression(node),
-            ast::NodeType::Integer => return node.token.literal.parse::<i64>().unwrap(),
-            ast::NodeType::Name => return self.lookup_name(node).value,
+            ast::NodeType::Integer => return match node.token.literal.parse::<i64>() {
+                Ok(value) => Ok(value),
+                Err(_) => Err(format!("lines:{}, column:{}, parse int failed", node.token.row, node.token.col)),
+            },
+            ast::NodeType::Name => return self.lookup_variable(node),
             _ => panic!("logic error"),
         }
     }
 
-    fn exec_print(&self, node: &ast::AstNode) {
+    fn exec_print(&self, node: &ast::AstNode) -> Result<(), String> {
         let expr = &node.childs[0];
-        let value = self.exec_expression(expr);
+        let value = match self.exec_expression(expr) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
         println!("{}", value);
+        Ok(())
     }
 
-    fn exec_assign(&mut self, node: &ast::AstNode) {
+    fn exec_assign(&mut self, node: &ast::AstNode) -> Result<(), String> {
         let name = &node.childs[0];
         let expr = &node.childs[1];
-        let value = self.exec_expression(expr);
+        let value = match self.exec_expression(expr) {
+            Ok(value) => value,
+            Err(msg) => return Err(msg),
+        };
         self.name_table.set(name.token.literal.clone(), ntable::Variable::new(&name.token.literal, value));
+        Ok(())
     }
 
     pub fn execute(&mut self, tree: &ast::AstNode) -> Result<(), String> {
         for node in &tree.childs {
             match node.node_type {
-                ast::NodeType::Print => self.exec_print(node),
-                ast::NodeType::Assign => self.exec_assign(node),
-                _ => return Err(format!("syntax error, expect 'print' or variable, line:{}, column:{}",
+                ast::NodeType::Print => match self.exec_print(node) {
+                    Ok(()) => (),
+                    Err(msg) => return Err(msg),
+                },
+                ast::NodeType::Assign => match self.exec_assign(node) {
+                    Ok(()) => (),
+                    Err(msg) => return Err(msg),
+                },
+                _ => return Err(format!("line:{}, column:{}, syntax error, expect 'print' or variable",
                             node.token.row, node.token.col)),
             }
         }
