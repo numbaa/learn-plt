943c4f71a3af6bbe7ebb6695c90dc1ec9dec1f76
Zhennan Tu <zhennan.tu@gmail.com>
2020-04-16 23:38:28 +0800

    execute function(not finished yet)

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,12 +1,17 @@
 use super::ast;
 use super::ntable;
 use super::tokenizer;
+use std::collections::HashMap;
 
 pub struct Interpreter {
     name_table: ntable::NameTable,
 }
 
 impl Interpreter {
+    fn exec_func_call(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<i64, String> {
+        //TODO: finish func call
+        Ok(0)
+    }
     fn lookup_variable(&self, node: &ast::AstNode) -> Result<i64, String> {
         return match self.name_table.get(&node.token.literal) {
             Ok(variable) => Ok(variable.value),
@@ -14,12 +19,12 @@ impl Interpreter {
         };
     }
 
-    fn exec_pow_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
-        let left = match self.exec_expression(&node.childs[0]) {
+    fn exec_pow_expression(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
-        let right = match self.exec_expression(&node.childs[1]) {
+        let right = match self.exec_expression(&node.childs[1], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
@@ -35,12 +40,12 @@ impl Interpreter {
         }
     }
 
-    fn exec_mul_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
-        let left = match self.exec_expression(&node.childs[0]) {
+    fn exec_mul_expression(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
-        let right = match self.exec_expression(&node.childs[1]) {
+        let right = match self.exec_expression(&node.childs[1], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
@@ -51,12 +56,12 @@ impl Interpreter {
         }
     }
 
-    fn exec_add_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
-        let left = match self.exec_expression(&node.childs[0]) {
+    fn exec_add_expression(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<i64, String> {
+        let left = match self.exec_expression(&node.childs[0], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
-        let right = match self.exec_expression(&node.childs[1]) {
+        let right = match self.exec_expression(&node.childs[1], func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
@@ -68,23 +73,24 @@ impl Interpreter {
         }
     }
 
-    fn exec_expression(&self, node: &ast::AstNode) -> Result<i64, String> {
+    fn exec_expression(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<i64, String> {
         match node.node_type {
-            ast::NodeType::Add => return self.exec_add_expression(node),
-            ast::NodeType::Mul => return self.exec_mul_expression(node),
-            ast::NodeType::Pow => return self.exec_pow_expression(node),
+            ast::NodeType::Add => return self.exec_add_expression(node, func_table),
+            ast::NodeType::Mul => return self.exec_mul_expression(node, func_table),
+            ast::NodeType::Pow => return self.exec_pow_expression(node, func_table),
             ast::NodeType::Integer => return match node.token.literal.parse::<i64>() {
                 Ok(value) => Ok(value),
                 Err(_) => Err(format!("lines:{}, column:{}, parse int failed", node.token.row, node.token.col)),
             },
             ast::NodeType::Name => return self.lookup_variable(node),
+            ast::NodeType::FuncCall => return self.exec_func_call(node, func_table),
             _ => panic!("logic error"),
         }
     }
 
-    fn exec_print(&self, node: &ast::AstNode) -> Result<(), String> {
+    fn exec_print(&self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<(), String> {
         let expr = &node.childs[0];
-        let value = match self.exec_expression(expr) {
+        let value = match self.exec_expression(expr, func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
@@ -92,10 +98,10 @@ impl Interpreter {
         Ok(())
     }
 
-    fn exec_assign(&mut self, node: &ast::AstNode) -> Result<(), String> {
+    fn exec_assign(&mut self, node: &ast::AstNode, func_table: &HashMap<String, &ast::AstNode>) -> Result<(), String> {
         let name = &node.childs[0];
         let expr = &node.childs[1];
-        let value = match self.exec_expression(expr) {
+        let value = match self.exec_expression(expr, func_table) {
             Ok(value) => value,
             Err(msg) => return Err(msg),
         };
@@ -103,14 +109,24 @@ impl Interpreter {
         Ok(())
     }
 
+    fn exec_func_decl<'a>(&mut self, node: &'a ast::AstNode, func_table: &mut HashMap<String, &'a ast::AstNode>) -> Result<(), String> {
+        func_table.insert(node.token.literal.clone(), node);
+        Ok(())
+    }
+
     pub fn execute(&mut self, tree: &ast::AstNode) -> Result<(), String> {
+        let mut func_table = HashMap::<String, &ast::AstNode>::new();
         for node in &tree.childs {
             match node.node_type {
-                ast::NodeType::Print => match self.exec_print(node) {
+                ast::NodeType::Print => match self.exec_print(node, &func_table) {
                     Ok(()) => (),
                     Err(msg) => return Err(msg),
                 },
-                ast::NodeType::Assign => match self.exec_assign(node) {
+                ast::NodeType::Assign => match self.exec_assign(node, &func_table) {
+                    Ok(()) => (),
+                    Err(msg) => return Err(msg),
+                },
+                ast::NodeType::FuncDecl => match self.exec_func_decl(node, &mut func_table) {
                     Ok(()) => (),
                     Err(msg) => return Err(msg),
                 },
