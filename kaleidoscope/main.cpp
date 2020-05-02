d5ead88c1a889247d982c2ceefff950878a50f47
Zhennan Tu <zhennan.tu@gmail.com>
2020-05-02 23:13:19 +0800

    parser

--- kaleidoscope/main.cpp
+++ kaleidoscope/main.cpp
@@ -1,7 +1,9 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <map>
 
+//lexer只支持ascii，认识的返回负数，不认识的返回[0-255]
 enum Token {
     tok_eof = -1,
     tok_def = -2,
@@ -19,6 +21,7 @@ static int gettok() {
     static int LastChar = ' ';
     while (isspace(LastChar))
         LastChar = getchar();
+    //identifier: [a-zA-Z][a-zA-Z0-9]*
     if (isalpha(LastChar)) {
         IdentifierStr = LastChar;
         while (isalnum((LastChar = getchar())))
@@ -29,6 +32,7 @@ static int gettok() {
             return tok_extern;
         return tok_identifier;
     }
+    //数字，只支持double，对于.12.34会识别错误
     if (isdigit(LastChar) || LastChar == '.') {
         std::string NumStr;
         do {
@@ -38,6 +42,7 @@ static int gettok() {
         NumVal = strtod(NumStr.c_str(), 0);
         return tok_number;
     }
+    //注释
     if (LastChar == '#') {
         do
             LastChar = getchar();
@@ -111,3 +116,233 @@ private:
     std::unique_ptr<PrototypeAST> Proto;
     std::unique_ptr<ExprAST> Body;
 };
+
+
+//trick，运算符优先级
+static std::map<char, int> BinopPrecedence;
+static int CurTok;
+static int getNextToken() {
+    return CurTok = gettok();
+}
+
+
+std::unique_ptr<ExprAST> LogError(const char* Str) {
+    fprintf(stderr, "LogError: %s\n", Str);
+    return nullptr;
+}
+
+std::unique_ptr<PrototypeAST> LogErrorP(const char* Str) {
+    LogError(Str);
+    return nullptr;
+}
+
+static std::unique_ptr<ExprAST> ParseExpression();
+
+static std::unique_ptr<ExprAST> ParserNumberExpr() {
+    auto Result = std::make_unique<NumberExprAST>(NumVal);
+    getNextToken();
+    return std::move(Result);
+}
+
+//括号并不需要展现在ast中
+static std::unique_ptr<ExprAST> ParseParenExpr() {
+    getNextToken();
+    auto V = ParseExpression();
+    if (!V)
+        return nullptr;
+    if (CurTok != ')')
+        return LogError("expected ')'");
+    getNextToken();
+    return V;
+}
+
+//标识，或 函数调用
+static std::unique_ptr<ExprAST> ParseIdentifierExpr() {
+    std::string IdName = IdentifierStr;
+
+    getNextToken();
+
+    if (CurTok != '(')
+        return std::make_unique<VariableExprAST>(IdName);
+    
+    getNextToken();
+    std::vector<std::unique_ptr<ExprAST>> Args;
+    if (CurTok != ')') {
+        while (true) {
+            if (auto Arg = ParseExpression())
+                Args.push_back(std::move(Arg));
+            else
+                return nullptr;
+            if (CurTok == ')')
+                break;
+            if (CurTok != ',')
+                return LogError("Expected ')' or ',' in argument list");
+            getNextToken();
+        }
+    }
+    getNextToken();
+    return std::make_unique<CallExprAST>(IdName, std::move(Args));
+}
+
+//标识（包括函数调用）、数字、括号
+static std::unique_ptr<ExprAST> ParsePrimary() {
+    switch (CurTok) {
+    case tok_identifier:
+        return ParseIdentifierExpr();
+    case tok_number:
+        return ParserNumberExpr();
+    case '(':
+        return ParseParenExpr();
+    default:
+        return LogError("unknown token when expecting an expression");
+    }
+}
+
+//获取运算符优先级，由外面保证当前tok是运算符
+static int GetTokPrecedence() {
+    if (!isascii(CurTok))
+        return -1;
+
+    int TokPrec = BinopPrecedence[CurTok];
+    if (TokPrec <= 0)
+        return -1;
+    return TokPrec;
+}
+
+static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<ExprAST> LHS) {
+    while (true) {
+        int TokPrec = GetTokPrecedence();
+
+        if (TokPrec < ExprPrec)
+            return LHS;
+        
+        int BinOp = CurTok;
+        getNextToken();
+
+        auto RHS = ParsePrimary();
+        if (!RHS)
+            return nullptr;
+        
+        //wow
+        int NextPrec = GetTokPrecedence();
+        if (TokPrec < NextPrec) {
+            RHS = ParseBinOpRHS(TokPrec + 1, std::move(RHS));
+            if (!RHS)
+                return nullptr;
+        }
+
+        LHS = std::make_unique<BinaryExprAST>(BinOp, std::move(LHS), std::move(RHS));
+    }
+}
+
+//表达式一定是 lhs op rhs ??
+static std::unique_ptr<ExprAST> ParseExpression() {
+    auto LHS = ParsePrimary();
+    if (!LHS)
+        return nullptr;
+    return ParseBinOpRHS(0, std::move(LHS));
+}
+
+static std::unique_ptr<PrototypeAST> ParsePrototype() {
+    if (CurTok != tok_identifier)
+        return LogErrorP("Expected function name in prototype");
+    
+    std::string FnName = IdentifierStr;
+    getNextToken();
+
+    if (CurTok != '(')
+        return LogErrorP("Expected '(' in prototype");
+
+    std::vector<std::string> ArgNames;
+    //没有逗号
+    while (getNextToken() == tok_identifier)
+        ArgNames.push_back(IdentifierStr);
+    if (CurTok != ')')
+        return LogErrorP("Expected ')' in prototype");
+    
+    getNextToken();
+    return std::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+}
+
+//单语句
+static std::unique_ptr<FunctionAST> ParseDefinition() {
+    getNextToken();
+    auto Proto = ParsePrototype();
+    if (!Proto)
+        return nullptr;
+    if (auto E = ParseExpression())
+        return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+    return nullptr;
+}
+
+//这是啥
+static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
+    if (auto E = ParseExpression()) {
+        auto Proto = std::make_unique<PrototypeAST>("__anon_expr", std::vector<std::string>());
+        return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+    }
+    return nullptr;
+}
+
+static std::unique_ptr<PrototypeAST> ParseExtern() {
+    getNextToken();
+    return ParsePrototype();
+}
+
+static void HandleDefinition() {
+    if (ParseDefinition()) {
+        fprintf(stderr, "Parsed a function defition.\n");
+    } else {
+        getNextToken();
+    }
+}
+
+static void HandleExtern() {
+    if (ParseExtern()) {
+        fprintf(stderr, "Parsed an extern\n");
+    } else {
+        getNextToken();
+    }
+}
+
+static void HandleTopLevelExpression() {
+    if (ParseTopLevelExpr()) {
+        fprintf(stderr, "Parsed a top-level expr\n");
+    } else {
+        getNextToken();
+    }
+}
+
+static void MainLoop() {
+    while (true) {
+        fprintf(stderr, "ready> ");
+        switch (CurTok) {
+        case tok_eof:
+            return;
+        case ';':   //??
+            getNextToken();
+            break;
+        case tok_def:
+            HandleDefinition();
+            break;
+        case tok_extern:
+            HandleExtern();
+            break;
+        default:
+            HandleTopLevelExpression();
+            break;
+        }
+    }
+}
+
+int main() {
+    BinopPrecedence['<'] = 10;
+    BinopPrecedence['+'] = 20;
+    BinopPrecedence['-'] = 20;
+    BinopPrecedence['*'] = 40;
+
+    fprintf(stderr, "ready> ");
+    getNextToken();
+    MainLoop();
+    return 0;
+}
